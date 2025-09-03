dfacf12b80d9ad4bc65f94a8cb9559008d66e243
Amdalat <hmayomikun@gmail.com>
2025-09-03 14:39:33 +0100

    uhhhhhh

---
+++ assets/js/index.js
@@ -0,0 +1,24 @@
+// const togglebtn = document.getElementById('hamburger');
+// const rownav = document.getElementById('rownavcont');
+// const columnnav = document.getElementById('columnnavcont');
+
+// togglebtn.addEventListener("click", ()=>{
+//     columnnav.lastElementChild.classList.toggle('hidediv');
+// })
+
+// function toggleNav(larger, smaller, width) {
+//     if (window.innerWidth <= width) {
+//       larger.classList.add("hidediv");
+//       smaller.classList.remove("hidediv");
+//     } else {
+//       larger.classList.remove("hidediv");
+//       smaller.classList.add("hidediv");
+//     }
+// }
+
+// window.addEventListener("load", ()=>{
+//   toggleNav(rownav, columnnav, 840)
+// });
+// window.addEventListener("resize", ()=>{
+//   toggleNav(rownav, columnnav, 840)
+// });
